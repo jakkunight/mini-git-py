082102471f239c91c167d913f32bf141ad0139b4
Jakku Night <J97k107u45k117n@gmail.com>
2025-10-26 21:59:04 -0300

    Agregar nombres de las referencias + Se permiten el uso de referencias nombradas.

--- src/mini_git_py/models/references.py
+++ src/mini_git_py/models/references.py
@@ -1,6 +1,10 @@
 class CommitReference:
-    def __init__(self, commit_hash: str):
+    def __init__(self, name: str, commit_hash: str):
         if commit_hash == "":
             raise TypeError("El hash provisto no puede estar vacío.")
 
+        if name == "":
+            raise TypeError("El nombre de la referencia no puede ser nulo.")
+
         self.commit_hash = commit_hash
+        self.name = name
