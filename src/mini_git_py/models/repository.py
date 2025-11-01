3c5f12c8897a40ecd52e5d9338f9b7d027d93adc
Jakku Night <J97k107u45k117n@gmail.com>
2025-11-01 20:04:02 -0300

    Remodelado del proyecto
    + Se remodelan los objetos siguiendo una mejor arquitectura.
    + Se aplica el patrón TypeState al proyecto.
    + Se aplica el patrón Repository al proyecto.
    + Se remodelan Tree, Blob, Commit y GitObject.
    + Se añade Tag.
    + Se añaden las clases Repository y Ref.
    
    - Inicialización del repositorio.
    - Almacenamiento y creación de referencias.
    - Inicialización y actualización del INDEX.
    - Actualización del HEAD.
    - Restore de archivos.
    - Conversión de GitObject a Commit.
    - Conversión de GitObject a Tree.
    - Conversión de GitObject a Tag.
    - Conversión de GitObject a Blob.
    - Conversiones a GitObject.

---
+++ src/mini_git_py/models/repository.py
@@ -0,0 +1,79 @@
+from mini_git_py.models.git_objects import GitObject
+from gzip import compress, decompress
+from os import path, makedirs, getcwd
+from dataclasses import dataclass
+
+
+@dataclass
+class Ref:
+    name: str
+    sha: str
+
+    def __post_init__(self):
+        assert self.name != ""
+        assert len(self.sha) == 32
+
+
+class Repository:
+    default_head_ref: str = "master"
+    content_encoding: str = "utf-8"
+    repository_dir: str | None = None
+    repository_store: str | None = None
+    object_store: str | None = None
+    refs_store: str | None = None
+    head_store: str | None = None
+    head_ref: str = default_head_ref
+    refs: list[str] = [default_head_ref]
+
+    def __init__(self):
+        pass
+
+    def init(self):
+        if self.repository_dir is not None:
+            return
+
+        self.repository_dir = getcwd()
+        self.repository_store: str = path.join(self.repository_dir, ".mg")
+        self.object_store: str = path.join(self.repository_store, "objects")
+        self.refs_store: str = path.join(self.repository_store, "refs")
+        self.head_store: str = path.join(self.repository_store, "HEAD")
+
+        makedirs(self.repository_store)
+        makedirs(self.object_store)
+        makedirs(self.refs_store)
+        head = open(self.head_store, "xb")
+        head.write(self.head_ref.encode(self.content_encoding))
+        head.close()
+
+    def store_object(self, object: GitObject):
+        field_separator: str = "\n"
+        part_separator: str = "\n\n"
+        header: str = f"{object.type}{field_separator}{object.size}"
+        object_content: str = f"{header}{part_separator}{object.content}"
+        compressed_bytes = compress(object_content.encode(self.content_encoding), 9)
+        file = open(
+            path.join(
+                self.repository_dir, self.object_store, object.sha[0:4], object.sha[4:]
+            ),
+            "wb",
+        )
+
+        file.write(compressed_bytes)
+        file.close()
+
+    def load_object(self, sha: str) -> GitObject:
+        field_separator: str = "\n"
+        part_separator: str = "\n\n"
+        file = open(
+            path.join(self.repository_dir, self.object_store, sha[0:4], sha[4:]), "rb"
+        )
+
+        compressed_bytes = file.read()
+        decompressed_bytes = decompress(compressed_bytes)
+        content_str = decompressed_bytes.decode(self.content_encoding)
+        [header, content] = content_str.split(part_separator)
+        [type, size] = header.split(field_separator)
+        return GitObject(sha, type, int(size), content.encode(self.content_encoding))
+
+    def add_ref(self):
+        pass
--- src/mini_git_py/models/tree.py
+++ src/mini_git_py/models/tree.py
@@ -1,55 +1,12 @@
-from mini_git_py.models.git_objects import GitObject, GitObjectType
-from hashlib import sha256
+from dataclasses import dataclass
 
 
-class TreeEntry:
-    def __init__(self, dirname: str):
-        self.mode = "040000"
-        self.dirname = dirname
+@dataclass
+class Tree:
+    name: str
+    type = "tree"
 
-
-class BlobEntry:
-    def __init__(self, filename: str):
-        self.mode = "100644"
-        self.filename = filename
-
-
-class Tree(GitObject):
-    def __init__(self, blob_entries: list[BlobEntry], tree_entries: list[TreeEntry]):
-        self.tree_entries = tree_entries
-        self.blob_entries = blob_entries
-
-    def get_contet(self) -> str:
-        content_str: str = ""
-        separator: str = "\n"
-        inner_separator: str = " "
-
-        content_str += self.get_type()
-        content_str += separator
-
-        for entry in self.tree_entries:
-            content_str += entry.mode
-            content_str += inner_separator
-            content_str += entry.dirname
-            content_str += separator
-
-        for entry in self.blob_entries:
-            content_str += entry.mode
-            content_str += inner_separator
-            content_str += entry.filename
-            content_str += separator
-
-        return content_str
-
-    def compute_hash(self) -> str:
-        content: str = self.get_content()
-
-        content_bytes: bytes = content.encode("utf-8")
-        hash = sha256(content_bytes)
-
-        hex_str: str = hash.hexdigest()
-
-        return hex_str
-
-    def get_type(self) -> GitObjectType:
-        return GitObjectType.TREE
+    def __post_init__(self):
+        assert self.name != "", """
+            El nombre provisto no puede ser vacío.
+        """
