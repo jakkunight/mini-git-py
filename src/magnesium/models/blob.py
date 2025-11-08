e2bfd2a83a31719a1dc3b6504fa69501bed92020
Jakku Night <J97k107u45k117n@gmail.com>
2025-11-08 17:18:29 -0300

    Reimplementación de LocalRepository + Se reimplementa LocalRepository. + Se hace una prueba parcial de la implementación.

--- src/magnesium/models/blob.py
+++ src/magnesium/models/blob.py
@@ -7,8 +7,7 @@ class Blob:
     Una clase que representa el contenido de un archivo sin ningún metadato. Sus campos pueden estar vacíos, a excepción del tipo, que es constante.
     """
 
-    mode: int
-    content: bytes
+    content: str
     type: str = "blob"
 
     def __post_init__(self):
